1dcddeedafa8ecf0688a47408c0f3ba7a16e314c
ygor xavier de sousa silva <68355711+YgorXavierS@users.noreply.github.com>
2023-03-30 20:44:28 -0300

    inicial

---
+++ JavaScript/1.js
@@ -0,0 +1,19 @@
+//PRIMEIROS PASSOS//
+//SAIDAS DE DADOS
+console.log("teste");
+
+/*VARIAVEIS
+const variavel = 10
+let variavel2 = 50
+*/
+
+//DESAFIO
+
+var priceGas = 5.79;
+var avg = 12;
+var distance = 1580;
+var priceTrip =(distance/avg)*priceGas
+
+console.log(priceTrip.toFixed(2));
+
+
